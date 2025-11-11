a40cebc4432cfe875c4303295793dc707addcdda
ComradeUa <az63.zov@gmail.com>
2025-11-11 12:01:46 +0200

    fix: fix fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { TagModule } from './tag/tag.module';
+import { PrismaModule } from './prisma/prisma.module';
+
 @Module({
-  imports: [AuthModule, UsersModule, TaskModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, AuthModule, UsersModule, TaskModule, ConfigModule.forRoot(), TagModule],
 })
 export class AppModule {}
